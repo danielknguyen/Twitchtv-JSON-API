57cc9a87337932e5cd2836384d572295dabd3ad4
Daniel Nguyen <danielnguyen93@hotmail.com>
2017-05-31 16:59:39 -0700

    added new background, modified ajax, centered buttons

--- script.js
+++ script.js
@@ -1,51 +1,83 @@
 $(document).ready(function(){
 	var twitchUsers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+	var isOnline = 'streams/';
+	var isOffline = 'channels/';
 
 	twitchUsers.map(function(data){
 		$.ajax({
-			url:'https://wind-bow.gomix.me/twitch-api/channels/' + data,
+			url:'https://wind-bow.gomix.me/twitch-api/' + isOnline + data,
 			dataType:'jsonp',
-			data:{
+			data: {
 				'callback':'?',
 			},
 			success: function(results){
 				// console.log(results);
-			
-				if (results.logo != null) {
-					imageSrc = results.logo;
-				} else {
-					imageSrc = 'https://vignette3.wikia.nocookie.net/java/images/0/0e/Camera_icon.gif/revision/latest?cb=20090227194712';
-				}
-
-				var logo = '<div class="img-div col-sm-2 col-md-2"><img class="user-icon" src="' + imageSrc + '"></div>';
-				var userStatus = results.status;
-				var userName = '<div class="user-div col-sm-2 col-md-2"><div class="user-username">' + results.display_name + '</div></div>';
-
-				function trucateString(str) {
-					var newString = "";
-					if (str.length > 35) {
-						newString = str.slice(0, 35) + ' ...';
+				if (results.stream != null) {
+					if (results.stream.channel.logo != null) {
+					imageSrc = results.stream.channel.logo;
 					} else {
-						newString = str;
+						imageSrc = 'https://vignette3.wikia.nocookie.net/java/images/0/0e/Camera_icon.gif/revision/latest?cb=20090227194712';
 					}
-					return newString;
-				};
 
-				if (results.status != null) {
-					userStatus = trucateString(userStatus);
+					var logo = '<div class="img-div col-sm-2 col-md-2"><img class="user-icon" src="' + imageSrc + '"></div>';
+					var userStatus = trucateString(results.stream.channel.status);
+					var userName = '<div class="user-div col-sm-2 col-md-2"><div class="user-username"><a class="twitch-link" target="_blank" href="' + results.stream.channel.url + '">' + results.stream.channel.display_name + '</a></div></div>';
+
+					function trucateString(str) {
+						var newString = "";
+						if (str.length > 35) {
+							newString = str.slice(0, 35) + ' ...';
+						} else {
+							newString = str;
+						}
+						return newString;
+					};
+					
+					var description = '<div class="user-description col-sm-8 col-md-8"><div class="twitch-description">' + userStatus + '</div></div>';
+
+					var users = '<div id="twitch-online" class="col-sm-12 col-md-12 text-center">';
+
+					users = users + logo + userName + description + '</div>';
+					
+					$('#twitch-container').append(users);
 				} else {
-					userStatus = 'Offline';
+					$.ajax({
+						url: 'https://wind-bow.gomix.me/twitch-api/' + isOffline + data,
+						dataType: 'JSONP',
+						data: {
+							'callback':'?'
+						},
+						success: function(results){
+							// console.log(results);
+
+							if (results.logo != null) {
+								imageSrc = results.logo;
+							} else {
+								imageSrc = 'https://vignette3.wikia.nocookie.net/java/images/0/0e/Camera_icon.gif/revision/latest?cb=20090227194712';
+							}
+
+							var logo = '<div class="img-div col-sm-2 col-md-2"><img class="user-icon" src="' + imageSrc + '"></div>';
+							var userStatus = 'Offline';
+							var userName = '<div class="user-div col-sm-2 col-md-2"><div class="user-username"><a class="twitch-link" target="_blank" href="' + results.url + '">' + results.name + '</a></div></div>';
+							var description = '<div class="user-description col-sm-8 col-md-8"><div class="twitch-description">' + userStatus + '</div></div>';
+
+							var users = '<div id="twitch-offline" class="col-sm-12 col-md-12 text-center">';
+
+							users = users + logo + userName + description + '</div>';
+
+							$('#twitch-container').append(users);
+						}
+					})
 				}
-
-				var description = '<div class="user-description col-sm-8 col-md-8"><div class="twitch-description">' + userStatus + '</div></div>';
-
-				var users = '<div id="twitch-online" class="col-sm-12 col-md-12 text-center">';
-
-				users = users + logo + userName + description + '</div>';
-				
-				$('html').append(users);
 			}
 		})
 	})
-
+	$('#online').on('click', function(){
+					('#twitch-offline').hide();
+					('#twitch-online').show();
+				})
+				$('#offline').on('click', function(){
+					('#twitch-online').hide();
+					('#twitch-offline').show();
+				})  
 });		
