63140071e1de20453231a905645a259d7cdd32a5
Daniel Nguyen <danielnguyen93@hotmail.com>
2017-06-01 13:39:04 -0700

    updated errors and bugs

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){
+
 	var twitchUsers = ["ESL_SC2", "OgamingSC2", "freecodecamp", "noobs2ninjas"];
 	var isOnline = 'streams/';
 	var isOffline = 'channels/';
 
-	twitchUsers.map(function(data){
+	twitchUsers.forEach(function(user){
+		runTwitchSearch(user);
+	})
+
+	function runTwitchSearch(user){
 		$.ajax({
-			url:'https://wind-bow.gomix.me/twitch-api/' + isOnline + data,
+			url:'https://wind-bow.gomix.me/twitch-api/' + isOnline + user,
 			dataType:'jsonp',
 			data: {
 				'callback':'?',
 			},
 			success: function(results){
 				// console.log(results);
+				
 				if (results.stream != null) {
 					if (results.stream.channel.logo != null) {
 					imageSrc = results.stream.channel.logo;
@@ -40,36 +46,73 @@ $(document).ready(function(){
 					users = users + logo + userName + description + '</div>';
 					
 					$('#twitch-container').append(users);
+
 				} else {
 					$.ajax({
-						url: 'https://wind-bow.gomix.me/twitch-api/' + isOffline + data,
+						url: 'https://wind-bow.gomix.me/twitch-api/' + isOffline + user,
 						dataType: 'JSONP',
 						data: {
 							'callback':'?'
 						},
 						success: function(results){
-							// console.log(results);
+							console.log(results);
 
-							if (results.logo != null) {
-								imageSrc = results.logo;
+							if (results.status === 422) {
+								var channelUnavailable = results.message;
+								$('#search-form')
+									.prepend('<p class="user-error text-center" style="color:red">' + channelUnavailable + '</p>')
+								$('.user-error')
+									.delay('3000')
+									.fadeOut('slow');
 							} else {
-								imageSrc = 'https://vignette3.wikia.nocookie.net/java/images/0/0e/Camera_icon.gif/revision/latest?cb=20090227194712';
+								if (results.logo != null) {
+								imageSrc = results.logo;
+								} else {
+									imageSrc = 'https://vignette3.wikia.nocookie.net/java/images/0/0e/Camera_icon.gif/revision/latest?cb=20090227194712';
+								}
+
+								var logo = '<div class="img-div col-sm-2 col-md-2"><img class="user-icon" src="' + imageSrc + '"></div>';
+								var userStatus = 'Offline';
+								var userName = '<div class="user-div col-sm-2 col-md-2"><div class="user-username"><a class="twitch-link" target="_blank" href="' + results.url + '">' + results.name + '</a></div></div>';
+								var description = '<div class="user-description col-sm-8 col-md-8"><div class="twitch-description">' + userStatus + '</div></div>';
+
+								var users = '<div id="twitch-offline" class="col-sm-12 col-md-12 text-center">';
+
+								users = users + logo + userName + description + '</div>';
+
+								$('#twitch-container').append(users);
 							}
-
-							var logo = '<div class="img-div col-sm-2 col-md-2"><img class="user-icon" src="' + imageSrc + '"></div>';
-							var userStatus = 'Offline';
-							var userName = '<div class="user-div col-sm-2 col-md-2"><div class="user-username"><a class="twitch-link" target="_blank" href="' + results.url + '">' + results.name + '</a></div></div>';
-							var description = '<div class="user-description col-sm-8 col-md-8"><div class="twitch-description">' + userStatus + '</div></div>';
-
-							var users = '<div id="twitch-offline" class="col-sm-12 col-md-12 text-center">';
-
-							users = users + logo + userName + description + '</div>';
-
-							$('#twitch-container').append(users);
 						}
 					})
 				}
 			}
-		})
-	}) 
+		}) 
+	}
+	
+	$('.twitch-search').keypress(function(e) {
+
+		var key = e.which || e.keyCode;
+		if(event.keyCode === 13) {
+			e.preventDefault();
+
+			var newUser = $('.twitch-search').val();
+
+			if (twitchUsers.indexOf(newUser) > -1) {
+				alert('Twitch user already exist')
+			} else {
+				runTwitchSearch(newUser);
+			}
+		}
+
+	})
+
+	$('.button-search').on('click',function(e){
+		e.preventDefault();
+		var newUser = $('.twitch-search').val();
+		if (twitchUsers.indexOf(newUser) > -1) {
+				alert('Twitch user already exist')
+			} else {
+				runTwitchSearch(newUser);
+			}
+	})
 });		
