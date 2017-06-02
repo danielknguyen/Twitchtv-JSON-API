c03f70684508f4c9b02c150319886450eb531bc6
Daniel Nguyen <danielnguyen93@hotmail.com>
2017-06-02 12:01:01 -0700

    searched for bugs and fixed them

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 				'callback':'?',
 			},
 			success: function(results){
-				// console.log(results);
+				console.log(results);
 				
 				if (results.stream != null) {
 					if (results.stream.channel.logo != null) {
@@ -27,7 +27,7 @@ $(document).ready(function(){
 
 					var logo = '<div class="img-div col-sm-2 col-md-2"><img class="user-icon" src="' + imageSrc + '"></div>';
 					var userStatus = trucateString(results.stream.channel.status);
-					var userName = '<div class="user-div col-sm-2 col-md-2"><div class="user-username"><a class="twitch-link" target="_blank" href="' + results.stream.channel.url + '">' + results.stream.channel.display_name.toLowerCase() + '</a></div></div>';
+					var userName = '<div class="user-div col-sm-2 col-md-2"><div class="user-username"><a class="twitch-link" target="_blank" href="' + results.stream.channel.url + '">' + results.stream.channel.display_name + '</a></div></div>';
 
 					function trucateString(str) {
 						var newString = "";
@@ -46,6 +46,8 @@ $(document).ready(function(){
 					users = users + logo + userName + description + '</div>';
 					
 					$('#twitch-container').append(users);
+					var newUser = $('.twitch-search').val().toLowerCase();
+					twitchUsers.push(newUser);
 
 				} else {
 					$.ajax({
@@ -55,37 +57,40 @@ $(document).ready(function(){
 							'callback':'?'
 						},
 						success: function(results){
-							// console.log(results);
+							console.log(results);
 
 							var channelUnavailable = results.message;
 
 							if (results.status === 400 || results.status === 401 || results.status === 403 || results.status === 404 || results.status === 422 || results.status === 429 || results.status === 500 || results.status === 503) {
 
-								$('#search-form')
-									.prepend('<p class="user-error text-center" style="color:red">*' + channelUnavailable + '*</p>');
-
 								$('.user-error')
-									.delay('3000')
-									.fadeOut('slow');
-
+									.text(channelUnavailable)
+									.show();
+								return false;									
 							} else {
-								if (results.logo != null) {
-								imageSrc = results.logo;
-								} else {
-									imageSrc = 'https://vignette3.wikia.nocookie.net/java/images/0/0e/Camera_icon.gif/revision/latest?cb=20090227194712';
-								}
-
-								var logo = '<div class="img-div col-sm-2 col-md-2"><img class="user-icon" src="' + imageSrc + '"></div>';
-								var userStatus = 'Offline';
-								var userName = '<div class="user-div col-sm-2 col-md-2"><div class="user-username"><a class="twitch-link" target="_blank" href="' + results.url + '">' + results.name.toLowerCase() + '</a></div></div>';
-								var description = '<div class="user-description col-sm-8 col-md-8"><div class="twitch-description">' + userStatus + '</div></div>';
-
-								var users = '<div id="twitch-offline" class="col-sm-12 col-md-12 text-center">';
-
-								users = users + logo + userName + description + '</div>';
-
-								$('#twitch-container').append(users);
+								$('.user-error')
+									.hide();
 							}
+
+							if (results.logo != null) {
+								imageSrc = results.logo;
+							} else {
+								imageSrc = 'https://vignette3.wikia.nocookie.net/java/images/0/0e/Camera_icon.gif/revision/latest?cb=20090227194712';
+							}
+
+							var logo = '<div class="img-div col-sm-2 col-md-2"><img class="user-icon" src="' + imageSrc + '"></div>';
+							var userStatus = 'Offline';
+							var userName = '<div class="user-div col-sm-2 col-md-2"><div class="user-username"><a class="twitch-link" target="_blank" href="' + results.url + '">' + results.name + '</a></div></div>';
+							var description = '<div class="user-description col-sm-8 col-md-8"><div class="twitch-description">' + userStatus + '</div></div>';
+
+							var users = '<div id="twitch-offline" class="col-sm-12 col-md-12 text-center">';
+
+							users = users + logo + userName + description + '</div>';
+
+							$('#twitch-container').append(users);
+							var newUser = $('.twitch-search').val().toLowerCase();
+							twitchUsers.push(newUser);
+							
 						}
 					})
 				}
@@ -98,20 +103,16 @@ $(document).ready(function(){
 		var key = e.which || e.keyCode;
 		if(event.keyCode === 13) {
 			e.preventDefault();
-
 			var newUser = $('.twitch-search').val().toLowerCase();
-			console.log(newUser);
+			if (newUser === "") {
+				return false;
 
-			if (twitchUsers.indexOf(newUser) > -1) {
-				$('#search-form')
-					.prepend('<p class="user-error text-center" style="color:red">*Twitch user already exist*</p>');
-
+			} else if (twitchUsers.indexOf(newUser) > -1) {
 				$('.user-error')
-					.delay('3000')
-					.fadeOut('slow');
+					.text('*Twitch user already exist*')
+					.show();
 			} else {
 				runTwitchSearch(newUser);
-				twitchUsers.push(newUser);
 			}
 		}
 
@@ -120,13 +121,15 @@ $(document).ready(function(){
 	$('.button-search').on('click',function(e){
 		e.preventDefault();
 		var newUser = $('.twitch-search').val().toLowerCase();
-		console.log(newUser);
-		if (twitchUsers.indexOf(newUser) > -1) {
-				$('#search-form')
-					.prepend('<p class="user-error text-center" style="color:red">*Twitch user already exist*</p>');
-			} else {
-				runTwitchSearch(newUser);
-				twitchUsers.push(newUser);
-			}
+		if (newUser === "") {
+			return false;
+
+		} else if (twitchUsers.indexOf(newUser) > -1) {
+			$('.user-error')
+				.text('*Twitch user already exist*')
+				.show();
+		} else {
+			runTwitchSearch(newUser);
+		}
 	})
 });		
